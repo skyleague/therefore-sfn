3146a2467c695cdc4d9391d6cd3d7b84393a41e2
Mick van Duijn <m.vanduijn@zefiros.io>
2022-09-30 17:44:36 +0200

    feat(cli): add cli for compiling stepfunction definitions

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,9 @@
+import type { StateMachine } from './sfn.type'
+
 export { ChoiceState } from './choice.type'
 export { FailState, MapState, ParallelState, PassState, StateMachine, SucceedState, TaskState, WaitState } from './sfn.type'
+
+export type SafeStateMachine<States extends StateMachine['States']> = Omit<StateMachine, 'StartAt' | 'States'> & {
+    States: States
+    StartAt: keyof States
+}
